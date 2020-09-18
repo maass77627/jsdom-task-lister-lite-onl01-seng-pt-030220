a1db0142222ee7bc43c6f1b851f15151298bf639
kelly r maass <kmaass77627@gmail.com>
2020-09-18 08:11:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-  document.getElementById("create-task-form").addEventListener("click", function(e) {
+  let form = document.getElementById("create-task-form").addEventListener("click", function(e) {
     e.preventDefault();
-    let form = document.getElementById("create-task-form")
+    
     let taskList = document.getElementById("tasks")
     let newTask = document.getElementById("new-task-description")
     newTask.select()
