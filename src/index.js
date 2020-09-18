f88ed1a209cc565896953c672f776a53d0415451
kelly r maass <kmaass77627@gmail.com>
2020-09-18 07:27:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.getElementById("create-task-form").addEventListener("click", function(e) {
     e.preventDefault();
+    let taskList = document.getElementById("tasks")
+    let newTask = document.getElementById("new-task-description")
+    taskList.appendChild(newTask)
   })
 });
