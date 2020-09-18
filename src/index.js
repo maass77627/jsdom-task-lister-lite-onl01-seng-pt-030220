b08b2088f1399bdb196d39ef53323b8e7efbe0f1
kelly r maass <kmaass77627@gmail.com>
2020-09-18 07:17:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.getElementById("create-task-form").addEventListener("click", function(e)) {
+    e.preventDefault()
+  }
 });
