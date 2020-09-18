70d6ead8f3a8fba1acdc4f10814e04f71619dc55
kelly r maass <kmaass77627@gmail.com>
2020-09-18 08:35:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 console.log("Hello World")
+const taskList = document.getElementById("tasks")
   const form = document.getElementById("create-task-form").addEventListener("submit", function(e) {
     e.preventDefault();
-
-    const taskList = document.getElementById("tasks")
-    const newTask = document.querySelector("#new-task-description").value
+   const newTask = document.querySelector("#new-task-description").value
     console.log(newTask)
-    //newTask.select()
+    
     li = document.createElement('li')
     li.innerHtml = "${newTask}"
     taskList.appendChild(newTask)
